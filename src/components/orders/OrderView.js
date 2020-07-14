5ed4d2b5e9bae117c95f8baf8028ebc65a22dade
Kaamuli Anthony Eriya <kaminto@outlook.com>
2020-07-14 13:55:18 +0300

    fix

--- src/components/orders/OrderView.js
+++ src/components/orders/OrderView.js
@@ -1,10 +1,10 @@
 import React from "react";
-// if (process.env.NODE_ENV === 'development') {
-//     const whyDidYouRender = require('@welldone-software/why-did-you-render');
-//     whyDidYouRender(React, {
-//         trackAllPureComponents: true,
-//     });
-// }
+if (process.env.NODE_ENV === 'development') {
+	const whyDidYouRender = require('@welldone-software/why-did-you-render');
+	whyDidYouRender(React, {
+		trackAllPureComponents: true,
+	});
+}
 import { View, StyleSheet } from 'react-native';
 import ProductListScreen from './ProductListScreen';
 import OrderSummaryScreen from "./OrderSummaryScreen";
@@ -14,19 +14,22 @@ import * as CustomerActions from "../../actions/CustomerActions";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import AppContext from '../../context/app-context';
-
+import orderItemStyles from "./orderItemStyles";
+import OrderSummary from './orderSummary';
+import OrderItems from './orderItems';
+import OrderCheckOut from './orderCheckOut';
 class OrderView extends React.PureComponent {
 	constructor(props) {
 		super(props);
 	}
 	static contextType = AppContext;
-	// static whyDidYouRender = true;
+	static whyDidYouRender = true;
 
 	render() {
 		return (
 			<View style={styles.orderView}>
 				<ProductListScreen />
-				<OrderSummaryScreen />
+				<OrderSummaryScreen/>
 			</View>
 		);
 	}
--- src/components/orders/orderCheckOutStyles.js
+++ src/components/orders/orderCheckOutStyles.js
@@ -1,116 +1,117 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
+
 const widthQuanityModal = '70%';
 const heightQuanityModal = 520;
 export default orderCheckOutStyles = StyleSheet.create({
-	onPayView: {
-		flex: 1,
-		justifyContent: 'center'
-	},
+  onPayView: {
+    flex: 1,
+    justifyContent: 'center',
+  },
 
-	btldesc: { textAlign: 'left', fontSize: 20, paddingLeft: 10 },
+  btldesc: { textAlign: 'left', fontSize: 20, paddingLeft: 10 },
 
-	btldesccont: { flex: 1, height: 45, flexDirection: 'row' },
-	onPayText: {
-		paddingTop: 10,
-		paddingBottom: 10,
-		textAlign: 'center'
-	},
-	container: {
-		flex: 1,
-		backgroundColor: "#2858a7",
+  btldesccont: { flex: 1, height: 45, flexDirection: 'row' },
+  onPayText: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    textAlign: 'center',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#2858a7',
 
-	},
+  },
 
-	checkBoxRow: {
-		flex: 1,
-		flexDirection: 'row',
-		marginTop: '1%',
-		alignItems: 'center'
-	},
-	checkBox: {},
-	checkLabel: {
-		left: 20,
-		fontSize: 20
-	},
-	totalText: {
-		marginTop: 10,
-		fontWeight: 'bold',
-		fontSize: 18,
-		color: 'black',
-		alignSelf: 'center'
-	},
+  checkBoxRow: {
+    flex: 1,
+    flexDirection: 'row',
+    marginTop: '1%',
+    alignItems: 'center',
+  },
+  checkBox: {},
+  checkLabel: {
+    left: 20,
+    fontSize: 20,
+  },
+  totalText: {
+    marginTop: 10,
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: 'black',
+    alignSelf: 'center',
+  },
 
-	buttonText: {
-		fontWeight: 'bold',
-		fontSize: 30,
-		alignSelf: 'center',
-		color: 'white'
-	},
-	summaryText: {
-		fontWeight: 'bold',
-		fontSize: 18,
-		color: 'black',
-		alignSelf: 'center'
-	},
-	baseItem: {
-		fontWeight: 'bold',
-		fontSize: 16,
-		color: 'black',
-		paddingTop: 4,
-		paddingBottom: 4,
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 30,
+    alignSelf: 'center',
+    color: 'white',
+  },
+  summaryText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: 'black',
+    alignSelf: 'center',
+  },
+  baseItem: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: 'black',
+    paddingTop: 4,
+    paddingBottom: 4,
 
-	},
+  },
 
-	modal2: {
-		justifyContent: 'center',
-		height: 300,
-		width: '65%',
-		padding: 5,
-		backgroundColor: "#f1f1f1"
-	},
+  modal2: {
+    justifyContent: 'center',
+    height: 300,
+    width: '65%',
+    padding: 5,
+    backgroundColor: '#f1f1f1',
+  },
 
-	completeOrder: {
-		backgroundColor: '#2858a7',
-		borderRadius: 10,
-		marginTop: '1%'
-	},
+  completeOrder: {
+    backgroundColor: '#2858a7',
+    borderRadius: 10,
+    marginTop: '1%',
+  },
 
-	completeOrderBtn: {
-		backgroundColor: '#2858a7',
-		bottom: 0,
-		marginTop: '3%',
-		marginBottom: 0,
-		// position: 'absolute'
-	},
+  completeOrderBtn: {
+    backgroundColor: '#2858a7',
+    bottom: 0,
+    marginTop: '3%',
+    marginBottom: 0,
+    // position: 'absolute'
+  },
 
-	modal3: {
-		justifyContent: 'center',
-		width: widthQuanityModal,
-		height: heightQuanityModal,
-	},
+  modal3: {
+    justifyContent: 'center',
+    width: widthQuanityModal,
+    height: heightQuanityModal,
+  },
 
-	headerItem: {
-		fontWeight: 'bold',
-		fontSize: 18,
-		color: 'black',
-		paddingTop: 5,
-		paddingBottom: 5,
-	},
+  headerItem: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: 'black',
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
 
-	headerBtlItem: {
-		fontWeight: 'bold',
-		fontSize: 16,
-		color: 'black',
-		paddingTop: 5,
-		paddingBottom: 5,
-	},
-	baseItem: {
-		fontWeight: 'bold',
-		fontSize: 16,
-		color: 'black',
-		paddingTop: 4,
-		paddingBottom: 4,
+  headerBtlItem: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: 'black',
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+  baseItem: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: 'black',
+    paddingTop: 4,
+    paddingBottom: 4,
 
-	},
+  },
 
 });
